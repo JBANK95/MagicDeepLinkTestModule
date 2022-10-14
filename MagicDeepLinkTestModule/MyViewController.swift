b761205ed0d4a3d5e2d62959346bae80318d0cf1
John Banks <jonathan.banks@albertsons.com>
2022-10-13 18:52:23 -0700

    no message

--- MagicDeepLinkTestModule/MyViewController.swift
+++ MagicDeepLinkTestModule/MyViewController.swift
@@ -1,7 +1,19 @@
 
+import ABMagicDeeplink
 
-class ViewController: UIViewController {
+class MyViewController: UIViewController, RoutingProtocol {
     
+    static let routerName = "MAIN_ROUTER"
+    static func route(to destination: RoutingDestination) {
+        switch destination.pushSection {
+        case "developmentpushsection":
+            print("**ROUTING TO MYUIVIEWCONTROLLER")
+            navigateToViewControllerFromHome(MyViewController.init(), asModal: false)
+            
+        default:
+            print("**DEFAULT")
+        }
+    }
     var blackSquare: UIView!
      
     override func viewDidLoad() {
